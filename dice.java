812181965723a284046b5fa6443dcd4f7b484a9f
Laura Malis <lemalis@crimson.ua.edu>
2021-10-04 23:42:30 -0700

    First commit

---
+++ dice.java
@@ -0,0 +1,29 @@
+class Dice {
+    
+    
+    
+    
+    public static void main(String[] args){
+        
+        
+        int firstDie;
+        int secondDie;
+        
+        
+        firstDie = (int)(Math.random()*6) + 1;
+        secondDie = (int)(Math.random()*6) + 1;
+    
+    
+        System.out.println(firstDie);
+        System.out.println(secondDie);
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
