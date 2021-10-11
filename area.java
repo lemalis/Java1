1d0dfabe30ded3fd84a9008237c1a440445708d6
Laura Malis <lemalis@crimson.ua.edu>
2021-10-10 20:06:10 -0700

    simple rectangle area calculation

---
+++ area.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+class area {
+
+public static void main(String[] args) {
+
+
+Scanner myObj = new Scanner(System.in);
+
+System.out.println("Enter area and width:");
+
+int length = myObj.nextInt();
+int width = myObj.nextInt();
+
+int area = length * width;
+
+System.out.println("Area = " + area);
+
+
+
+
+}
+
+
+
+
+}
